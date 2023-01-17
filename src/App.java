f9214fb59afc0e64055f87ffb24ff6bbd914f35f
aibii <aybiykeomurzakova@gmail.com>
2023-01-17 15:06:52 -0500

    message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
-        System.out.println(x: "Test ");
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File("res/grades.csv"));
+            String headerLine = fileScanner.nextLine();
+            headerLine = headerLine.replaceAll("\"", "");
+            String[]headers = headerLine.split(",");
+
+            FileWriter fileWriter = new FileWriter("res/grades.html");
+
+            StringBuilder builder = new StringBuilder();
+            builder.append("table").append("\n");
+            builder.append("<tr>");
+            for(int i = 0; i < headers.length; i++)
+            {
+                builder.append("<th>").append(headers[i]).append("</th>").append("\n");
+            }
+            builder.append("</tr>").append("\n");
+            builder.append("</table>");
+
+            fileWriter.append(builder.toString());
+            fileWriter.close();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
